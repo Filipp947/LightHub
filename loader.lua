a660cfb8b513cdfb90ec71ff9fe57a70a44d10ea
Filipp947 <filipp69420@gmail.com>
2025-08-01 18:23:34 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 -- skidder caught in 4k ultra hd, dont skid bro its useless i obf everything
 
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/bf1875bb07e24bb9bc8dd684e1afbc5e"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/e35ac2fbbed448cea30b5a2dc16e9654"))()
