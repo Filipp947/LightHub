4d7d3a922e5ec91fe5644119e98f96b974238f29
Filipp947 <filipp69420@gmail.com>
2025-10-29 13:53:26 +0300

    pcb

--- pvb.lua
+++ pvb.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/16b47755499bee1ef4e68f0817c0efa76ed094cddd09cf0c5ba917aec550a567/download"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f020a6c36f648cab3f8cee0eebdd3930.lua"))()
