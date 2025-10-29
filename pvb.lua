e41f3fa877fe2bfc929820f4abcc28a0c75f6e73
Filipp947 <filipp69420@gmail.com>
2025-10-29 16:35:11 +0300

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f020a6c36f648cab3f8cee0eebdd3930.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Filipp947/LightHub/refs/heads/main/loader.lua"))()
